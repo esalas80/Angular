f3f59f219a6240b7be3d16cec660ba227d8b3919
Ernesto Valencia <e.salas80@gmail.com>
2023-04-17 13:24:49 -0600

    comit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { ProtegidaComponent } from './components/protegida/protegida.component';
+import { PreciosComponent } from './components/precios/precios.component';
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home', component: HomeComponent},
+  {path:'precios', component: PreciosComponent},
+  {path:'protegida', component: ProtegidaComponent},
+  {path:'**', pathMatch:'full', redirectTo:'home'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
